b92082104c419eba0a9b9d8e9a1b42c8bf67b379
zhangyi <zhangyi@corp-ci.com>
2021-11-13 21:25:27 +0800

    finish linked list

--- algorithm/linked-list/web/linked-list.js
+++ algorithm/linked-list/web/linked-list.js
@@ -1,21 +1,104 @@
-var List = /** @class */ (function () {
-    function List() {
+class Node {
+  next
+  data
+
+  constructor(data) {
+    this.data = data
+    this.next = null
+  }
+}
+
+class List {
+  start = null; // 头结点
+  end = null; // 尾节点
+
+  add(data) {
+    const node = new Node(data)
+    if (!this.start) {
+      this.start = node
     }
-    List.prototype.add = function (data) {
-        var list = new List();
-        list.data = data;
-        if (!this.start) {
-            this.start = list;
+
+    if (this.end) {
+      this.end.next = node
+    }
+
+    this.end = node
+  }
+
+  delete(val) {
+    let cur = this.start
+    let pre
+    while(cur) {
+      if (cur.data === val) {
+        // 单节点
+        if (this.start === this.end) {
+          this.start = null
+          this.end = null
+          break
         }
-        list.start = this;
-        if (!this.end) {
-            this.end = list;
+
+        if (pre) {
+          pre.next = cur.next
+        } else {
+          // 删第一个节点，处理start
+          if (cur.next) {
+            this.start = cur.next
+          }
         }
-        else {
-            this.end.start = list;
-            list.end = this.end;
-            this.end = list;
+
+        // 处理最后一个节点
+        if (!cur.next && pre) {
+          this.end = pre
         }
-    };
-    return List;
-}());
+        break
+      }
+      pre = cur
+      cur = cur.next
+    }
+  }
+
+  insertAfter(anchor, val) {
+    let cur = this.start
+    while(cur) {
+      if (cur.data === anchor) {
+        const node = new Node(val)
+        if (cur.next) {
+          node.next = cur.next
+        } else {
+          this.end = node
+        }
+        cur.next = node
+        break
+      }
+      cur = cur.next
+    }
+  }
+
+  item(index) {
+    let i = 0
+    let cur = this.start
+    while(cur) {
+      i++
+      if (i === index) {
+        return cur
+      }
+      cur = cur.next
+    }
+    return null
+  }
+
+  each(cb) {
+    let cur = this.start
+    while(cur) {
+      cb(cur)
+      cur = cur.next
+    }
+  }
+
+}
+
+window.List = List
+
+
+
+
